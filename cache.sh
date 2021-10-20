8adff1a68fa3f225c1c1c9633870b0229fda4ea5
Piper McCorkle <piper@ladderlife.com>
2021-10-19 19:06:45 -0500

    Reference GitHub flake in cache.sh

--- cache.sh
+++ cache.sh
@@ -6,20 +6,21 @@
 set -euxo pipefail
 
 CACHIX_CACHE="${CACHIX_CACHE:-ladderlife-buck}"
+FLAKE="${FLAKE:-github:LadderLife/buck/piper/dev-with-async}"
 
 # See https://docs.cachix.org/pushing#flakes
 
 # Cache build dependencies
-cachix watch-exec "$CACHIX_CACHE" nix -- --experimental-features 'nix-command flakes' build
+cachix watch-exec "$CACHIX_CACHE" nix -- --experimental-features 'nix-command flakes' build "$FLAKE"
 # Cache inputs
-nix --experimental-features 'nix-command flakes' flake archive --json \
+nix --experimental-features 'nix-command flakes' flake archive "$FLAKE" --json \
   | jq -r '.path,(.inputs|to_entries[].value.path)' \
   | cachix push "$CACHIX_CACHE"
 # Cache artifacts
-nix --experimental-features 'nix-command flakes' build --json \
+nix --experimental-features 'nix-command flakes' build "$FLAKE" --json \
   | jq -r '.[].outputs | to_entries[].value' \
   | cachix push "$CACHIX_CACHE"
 # Cache dev shell
-nix --experimental-features 'nix-command flakes' develop --profile dev-profile --command true
+nix --experimental-features 'nix-command flakes' develop "$FLAKE" --profile dev-profile --command true
 cachix push "$CACHIX_CACHE" dev-profile
 rm dev-profile*
